368d0a6552fa4b514ae363c941ac14562261fbd5
Daniel Franco <daniel.franco.inbox@gmail.com>
2026-01-09 02:13:22 +0000

    fix: image names

--- .config/wezterm/bg.lua
+++ .config/wezterm/bg.lua
@@ -2,8 +2,8 @@ local BG = {}
 local home = os.getenv("HOME")
 
 -- backgrounds
-BG.blurred = home .. "/dotfiles/.config/wezterm/assets/wezterm_bg_blurred.png"
-BG.dark_blurred = home .. "/dotfiles/.config/wezterm/assets/wezterm_bg_blurred.png"
+BG.blurred = home .. "/dotfiles/.config/wezterm/assets/bg_blurred.png"
+BG.dark_blurred = home .. "/dotfiles/.config/wezterm/assets/bg_dark_blurred.png"
 
 -- set your default background here
 BG.default = BG.blurred
