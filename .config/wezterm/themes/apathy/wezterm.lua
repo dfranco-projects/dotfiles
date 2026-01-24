c7b7e0528b48563b047db78c282b3247be03c854
Daniel Franco <daniel.franco.inbox@gmail.com>
2026-01-24 22:20:28 +0000

    updated themes, zshrc, makefile

--- .config/wezterm/themes/apathy/wezterm.lua
+++ .config/wezterm/themes/apathy/wezterm.lua
@@ -2,8 +2,7 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("MesloLGS Nerd Font Mono", { weight = "Regular" })
-
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 18
 
 config.enable_tab_bar = false
