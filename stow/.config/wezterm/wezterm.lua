6f5065c61f590495417d1481d77af230aa58913f
Daniel Franco <daniel.franco.inbox@gmail.com>
2026-01-25 20:02:24 +0000

    updated wezterm.lua

--- stow/.config/wezterm/wezterm.lua
+++ stow/.config/wezterm/wezterm.lua
@@ -1,26 +1,16 @@
 local wezterm = require("wezterm")
-
 local config = wezterm.config_builder()
+local home = os.getenv("HOME")
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 19
+config.font_size = 16
+config.line_height = 1
 
 config.enable_tab_bar = false
-
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 10
+config.window_background_image = home .. "/.config/wezterm/assets/bg_blurred.png"
 
-config.colors = {
-	foreground = "#CBE0F0",
-	background = "#011423",
-	cursor_bg = "#47FF9C",
-	cursor_border = "#47FF9C",
-	cursor_fg = "#011423",
-	selection_bg = "#033259",
-	selection_fg = "#CBE0F0",
-	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
-}
+config.max_fps = 120
+config.native_macos_fullscreen_mode = true
 
 return config
