fa9252b1acb9ed1b71608dfea504d5612b6b0f3c
Daniel Franco <daniel.franco.inbox@gmail.com>
2026-05-28 13:29:39 +0100

    feat(wezterm): switch to kanagawa dragon theme and hide tab bar

--- stow/.config/wezterm/wezterm.lua
+++ stow/.config/wezterm/wezterm.lua
@@ -5,14 +5,13 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 18
 
+config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 
-config.color_scheme = [[Gruvbox dark, hard (base16)]]
+config.color_scheme = [[Kanagawa Dragon (Gogh)]]
 
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 10
-
-config.send_composed_key_when_left_alt_is_pressed = true
 
 require("keys").apply(config)
 require("tabs").apply(config)
